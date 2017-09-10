0f03eb06391a6acbfe55223e9ec996214bf2aca7
Wojciech Czabański <porke90@gmail.com>
2017-09-10 23:03:42 +0200

    Update euler_10.hs

--- lab_1/group/euler_10.hs
+++ lab_1/group/euler_10.hs
@@ -2,12 +2,13 @@
 -- Exercise: Euler 10
 -- Authors: Quinten Heijn, Dylan Bartels, Wojciech Czabański
 --------------------------------------------------------------------------
+-- The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+-- Find the sum of all the primes below two million.
+--------------------------------------------------------------------------
 
 module Euler_10 where
 import Data.Numbers.Primes
 
--- The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
--- Find the sum of all the primes below two million.
 
 primesUntil :: Integer -> [Integer]
 primesUntil lim = takeWhile (<lim) primes
