00ae75087796c0447383fbba69370798da7d7f1e
Wojciech Czabański <porke90@gmail.com>
2017-09-10 23:02:10 +0200

    Create euler_9.hs

---
+++ lab_1/group/euler_9.hs
@@ -0,0 +1,28 @@
+-- Assignment: Lab1
+-- Exercise: 1
+-- Authors: Quinten Heijn, Dylan Bartels, Wojciech Czabański
+--------------------------------------------------------------------------
+-- A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+-- a2 + b2 = c2
+-- For example, 32 + 42 = 9 + 16 = 25 = 52.
+-- There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+-- Find the product abc.
+--------------------------------------------------------------------------
+
+module Euler_9 where
+
+isTriplet :: Int -> Int -> Int -> Bool
+isTriplet a b c = (a^2 + b^2) == c^2
+
+tupleProduct :: (Int, Int, Int) -> Int
+tupleProduct (a, b, c) = a * b * c
+
+findTriplet :: Int -> Int -> Int -> Int -> (Int, Int, Int)
+findTriplet a b c lim | isTriplet a b c && a + b + c == 1000 = (a, b, c)
+                      | a < b = findTriplet (a+1) b c lim
+                      | b < c = findTriplet 1 (b+1) c lim
+                      | c < lim = findTriplet 1 1 (c+1) lim
+                      | otherwise = (-1, -1, -1)
+
+solution :: Int -> Int
+solution n = tupleProduct (findTriplet 1 1 1 n)
