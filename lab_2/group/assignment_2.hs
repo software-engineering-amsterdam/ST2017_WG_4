fa2bd8a38dabd694ba1513ec1fff0f5572bb1314
Dylan <dylan.j.bartels@gmail.com>
2017-09-17 18:04:02 +0200

    Moved ass2 to group

--- lab_2/group/assignment_2.hs
+++ lab_2/group/assignment_2.hs
@@ -1,36 +1,86 @@
--- Assignment: Lab2
--- Exercise: 2
--- Authors: Quinten Heijn, Dylan Bartels,
---          Wojciech Czabański, Elias El Khaldi Ahanach
---------------------------------------------------------------------------------
-
-module Lab2_2 where
-
 import Data.List
 import Data.Char
 import System.Random
 import Test.QuickCheck
 
--- Recognizing triangles
-
 data Shape = NoTriangle | Equilateral
-            | Isosceles  | Rectangular | Other deriving (Eq,Show)
+           | Isosceles  | Rectangular | Other deriving (Eq,Show)
 
 triangle :: Integer -> Integer -> Integer -> Shape
-triangle a b c | a == b && b == c = Equilateral
-               | a^2 + b^2 == c^2 || a^2 + c^2 == b^2 || b^2 + c^2 == b^2 = Rectangular
-               | a == b || b == c = Isosceles
-               | a + b < c || a + c < b || b + c < a = NoTriangle
-               | otherwise = Other
+triangle x y z
+  | x + y <= z || x + z <= y || y + z <= x                   = NoTriangle
+  | x == y && y == z                                         = Equilateral
+  | x == y || y == z || z == x                               = Isosceles
+  | x^2 + y^2 == z^2 || x^2 + z^2 == y^2 || y^2 + z^2 == x^2 = Rectangular
+  | otherwise                                                = Other
 
-test :: Positive Integer -> Positive Integer -> Positive Integer -> Bool
-test a b c = False
+-- Shape indentify functions
 
--- TODO: Create a test function
--- Create generators for each type of triangle
--- For a [[Int, Int, Int]]
+isEquilateral :: Integer -> Integer -> Integer -> Bool
+isEquilateral x y z
+  | (x == y) && (y == z) = True
+  | otherwise            = False
 
--- There is not posibility of Rectangular triangle
--- Overleaf
+isRectangular :: Integer -> Integer -> Integer -> Bool
+isRectangular x y z
+  | x^2 + y^2 == z^2 || x^2 + z^2 == y^2 || y^2 + z^2 == x^2 = True
+  | otherwise                                                = False
 
--- Time spent: 10 minutes
+isIsosceles :: Integer -> Integer -> Integer -> Bool
+isIsosceles x y z
+  | (x == y) && (x /= z) || (y == z) && (y /= x) || (z == x) && (z /= y) = True
+  | otherwise                                                            = False
+
+isNoTriangle :: Integer -> Integer -> Integer -> Bool
+isNoTriangle x y z
+  |  x + y < z || x + z < y || y + z < x = True
+  | otherwise                            = False
+
+-- Shape generation functions
+
+-- > genEquilaterals 10
+genEquilaterals :: Integer -> [[Integer]]
+genEquilaterals n = [[x,x,x] | x <- [1..n]]
+
+-- > genRectangulars 10
+genRectangulars :: Integer -> [[Integer]]
+genRectangulars n = [[x,y,z] | x <- [1..n],
+                               y <- [1..n],
+                               z <- [1..n],
+                               (x^2) + (y^2) == (z^2)]
+-- > genIsosceless 10
+genIsosceles :: Integer -> [[Integer]]
+genIsosceles n = [[x,x,y] | x <- [1..n],
+                            y <- [1..n],
+                            x /= y,
+                            2 * x > y]
+-- > genNoTriangles 10
+genNoTriangles :: Integer -> [[Integer]]
+genNoTriangles n = [[x,y,z] | x <- [0..n],
+                              y <- [0..(x `div` 2)],
+                              z <- [0..(x `div` 2)]]
+
+-- Tests
+
+testEquilateral :: Bool
+testEquilateral = testHelper 100 genEquilaterals Equilateral
+
+testRectangular :: Bool
+testRectangular = testHelper 100 genRectangulars Rectangular
+
+testIsoceles :: Bool
+testIsoceles = testHelper 100 genIsosceles Isosceles
+
+testNoTriangles :: Bool
+testNoTriangles = testHelper 100 genNoTriangles NoTriangle
+
+testHelper :: Integer -> (Integer -> [[Integer]]) -> Shape -> Bool
+testHelper x y z = all (\x -> (triangle (x !! 0) (x !! 1) (x !! 2)) == z) (y x)
+
+-- Main
+
+mainTest :: Bool
+mainTest = testEquilateral &&
+           testRectangular &&
+           testIsoceles &&
+           testNoTriangles
