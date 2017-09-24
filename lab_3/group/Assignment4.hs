427489ce0b2197554eef0f8bc3049d8ab5ef11e4
Dylan <dylan.j.bartels@gmail.com>
2017-09-24 13:07:28 +0200

    Cleaned form generator

--- lab_3/group/Assignment4.hs
+++ lab_3/group/Assignment4.hs
@@ -2,41 +2,51 @@
 -- Exercise: 4
 -- Authors: Quinten Heijn, Dylan Bartels,
 --          Wojciech Czabański, Elias El Khaldi Ahanach
--- Time needed: 
+-- Time needed:
 --------------------------------------------------------------------------
 
-module Assignment4 where
+module Lab3 where
 import Data.List
 import Control.Monad
 import Test.QuickCheck
 import Lecture3
 
+-- Generate bound form:
+arbitrarySizedForm :: Int -> Gen Form
+arbitrarySizedForm 0       = do
+  t  <- arbitrary
+  return (Prop t)
+arbitrarySizedForm n | n>0 =
+  oneof [liftM Neg subForm,
+         liftM Cnj subFormBracets,
+         liftM Dsj subFormBracets,
+         liftM2 Impl subForm subForm,
+         liftM2 Equiv subForm subForm]
+       where subForm        = arbitrarySizedForm (n `div` 2)
+             subFormBracets = listOf (arbitrarySizedForm (n `div` 4))
+
+
 instance Arbitrary Form where
-    arbitrary = oneof [arbitraryProp, arbitraryNeg,
-                       arbitraryProp, arbitraryCnj,
-                       arbitraryProp, arbitraryDsj,
-                       arbitraryProp, arbitraryImpl,
-                       arbitraryEquiv]
-       where arbitraryProp = do
-               x <- arbitrary
-               return $ Prop x
-             arbitraryNeg = do
-               x <- (arbitrary :: Gen Form)
-               return $ Neg x
-             arbitraryCnj = do
-               x <- (arbitrary :: Gen Form)
-               return $ Cnj [x]
-             arbitraryDsj = do
-               x <- (arbitrary :: Gen Form)
-               return $ Dsj [x]
-             arbitraryImpl = do
-               x <- (arbitrary :: Gen Form)
-               y <- (arbitrary :: Gen Form)
-               return $ Impl x y
-             arbitraryEquiv = do
-               x <- (arbitrary :: Gen Form)
-               y <- (arbitrary :: Gen Form)
-               return $ Equiv x y
+  arbitrary = sized arbitrarySizedForm
+-- > generate arbitrary :: IO (Form Int)
+-- > sample $ (arbitrary :: Gen (Form Int))
+
+-- Testable Propertys
 
 prop_isSatisfiable :: Form -> Bool
 prop_isSatisfiable x = satisfiable x
+-- > quickCheck (forAll (arbitrary :: Gen Form) prop_isSatisfiable)
+
+test :: IO ()
+test =
+ do quickCheck(prop_isSatisfiable)
+
+     -- Other Tests
+
+
+    return ()
+
+main :: IO ()
+main =
+ do test
+    return ()
--- lab_3/individual/dylan_bartels/lab3_4.hs
+++ lab_3/individual/dylan_bartels/lab3_4.hs
@@ -8,49 +8,44 @@ module Lab3 where
 import Data.List
 import Control.Monad
 import Test.QuickCheck
-
-type Name = Int
-
--- Needs to have 'a' to scale length of generated Form with input from quickcheck
-data Form a = Prop Name
-          | Neg (Form a)
-          | Cnj (Form a)
-          | Dsj (Form a)
-          | Impl (Form a) (Form a)
-          | Equiv (Form a) (Form a)
-          deriving (Eq,Ord)
-
-instance Show (Form a) where
-  show (Prop x)   = show x
-  show (Neg x)    = '-' : show x
-  show (Cnj fs)     = "*(" ++ showLst [fs] ++ ")"
-  show (Dsj fs)     = "+(" ++ showLst [fs] ++ ")"
-  show (Impl f1 f2)  = "(" ++ show f1 ++ "==>"
-                           ++ show f2 ++ ")"
-  show (Equiv f1 f2)  = "(" ++ show f1 ++ "<=>"
-                           ++ show f2 ++ ")"
-
-showLst,showRest :: [Form a] -> String
-showLst [] = ""
-showLst (f:fs) = show f ++ showRest fs
-showRest [] = ""
-showRest (f:fs) = ' ': show f ++ showRest fs
+import Lecture3
 
 -- Generate bound form:
-arbitrarySizedForm :: Arbitrary a => Int -> Gen (Form a)
+arbitrarySizedForm :: Int -> Gen Form
 arbitrarySizedForm 0       = do
   t  <- arbitrary
   return (Prop t)
 arbitrarySizedForm n | n>0 =
   oneof [liftM Neg subForm,
-         liftM Cnj subForm,
-         liftM Dsj subForm,
+         liftM Cnj subFormBracets,
+         liftM Dsj subFormBracets,
          liftM2 Impl subForm subForm,
          liftM2 Equiv subForm subForm]
-       where subForm = arbitrarySizedForm (n `div` 2)
+       where subForm        = arbitrarySizedForm (n `div` 2)
+             subFormBracets = listOf (arbitrarySizedForm (n `div` 4))
 
 
-instance (Arbitrary a) => Arbitrary (Form a) where
+instance Arbitrary Form where
   arbitrary = sized arbitrarySizedForm
 -- generate arbitrary :: IO (Form Int)
 -- sample $ (arbitrary :: Gen (Form Int))
+
+-- Testable Propertys
+
+prop_isSatisfiable :: Form -> Bool
+prop_isSatisfiable x = satisfiable x
+-- > quickCheck (forAll (arbitrary :: Gen Form) prop_isSatisfiable)
+
+test :: IO ()
+test =
+ do quickCheck(prop_isSatisfiable)
+
+     -- Other Tests
+
+
+    return ()
+
+main :: IO ()
+main =
+ do test
+    return ()
