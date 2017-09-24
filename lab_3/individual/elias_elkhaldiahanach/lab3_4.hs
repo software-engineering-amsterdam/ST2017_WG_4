235392ba67df22900c727e51ef452a8fd365cb77
Elias <elias.el.khaldi@gmail.com>
2017-09-24 18:33:03 +0200

    testOperators added

--- lab_3/individual/elias_elkhaldiahanach/lab3_4.hs
+++ lab_3/individual/elias_elkhaldiahanach/lab3_4.hs
@@ -9,4 +9,19 @@ import Data.List
 import Test.QuickCheck
 import Lecture3
 
+-- Test whether a form has the operators allowed for CNF. Also checks whether the only Conjunction is the one in the beginning ("*(...)")
+testOperators :: Form -> Bool
+testOperators (Cnj []) = True
+testOperators (Cnj (h:t)) = testOperators h && testOperators (Cnj t) 
+testOperators x = testOperators2 x
 
+-- Tests whether the inside ("*(..inside..)") of the formula does not contain forbidden operators
+testOperators2 :: Form -> Bool
+testOperators2 (Cnj x) = False
+testOperators2 (Dsj []) = True
+testOperators2 (Dsj (h:t)) = testOperators2 h && testOperators2 (Dsj t) 
+testOperators2 (Neg (Prop x)) = True
+testOperators2 (Neg (x)) = False
+testOperators2 (Prop x) = True
+testOperators2 (Impl x y) = False
+testOperators2 (Equiv x y) = False
