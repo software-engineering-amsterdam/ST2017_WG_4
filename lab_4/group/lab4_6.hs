a747943b5ed0021464377dc32fd2f9622f1abc09
Elias <elias.el.khaldi@gmail.com>
2017-10-01 16:17:59 +0200

    update group

--- lab_4/group/lab4_6.hs
+++ lab_4/group/lab4_6.hs
@@ -2,7 +2,7 @@
 -- Exercise: 6
 -- Authors: Quinten Heijn, Dylan Bartels,
 --          Wojciech Czabański, Elias El Khaldi 
--- Time needed: 20 minutes
+-- Time needed: 30 minutes
 --------------------------------------------------------------------------
 -- Use the datatype for relations from the previous exercise, plus {...} to define a function
 -- trClos :: Ord a => Rel a -> Rel a 
@@ -25,16 +25,19 @@ infixr 5 @@
 r @@ s = 
  nub [ (x,z) | (x,y) <- r, (w,z) <- s, y == w ]
 
--- Informal description transitive closure:
--- If there is a path from a to b, then there should be an edge from a to b
-trClos :: Ord a => Rel a -> Rel a 
-trClos x = trClos2 (length x) x
-
-trClos2 :: Ord a => Int -> Rel a -> Rel a
-trClos2 i x | (length (noDup(x ++ (x @@ x)))) == i = x | otherwise = trClos2 (length (noDup(x ++ (x @@ x)))) (noDup(x ++ (x @@ x)))
-
+--
 noDup :: Ord a => Rel a -> Rel a
 noDup lst = set2list(list2set lst)
 
 set2list :: Ord a => Set a -> [a]
 set2list (Set lst) = lst
+--
+ 
+-- Informal description transitive closure:
+-- If there is a path from a to b, then there should be an edge from a to b
+trClos :: Ord a => Rel a -> Rel a 
+trClos x = trClos2 (length x) x
+-- Helping function
+trClos2 :: Ord a => Int -> Rel a -> Rel a
+trClos2 i x | (length (noDup(x ++ (x @@ x)))) == i = x | otherwise = trClos2 (length (noDup(x ++ (x @@ x)))) (noDup(x ++ (x @@ x)))
+
