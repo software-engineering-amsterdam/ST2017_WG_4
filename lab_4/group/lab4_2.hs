bef6d7177ac996eb675404cb27b2faaf481e54b1
Dylan Bartels <d_bartels@live.nl>
2017-10-01 12:38:43 +0200

    Cleanup code

--- lab_4/group/lab4_2.hs
+++ lab_4/group/lab4_2.hs
@@ -10,7 +10,11 @@
 
 -- (Deliverables: two random test generators, indication of time spent.)
 
+-- Information used
 -- https://hackage.haskell.org/package/QuickCheck-2.10.0.1/docs/Test-QuickCheck-Gen.html
+-- if/then/else for IO Bool https://mail.haskell.org/pipermail/haskell-cafe/2006-November/019752.html
+-- https://stackoverflow.com/questions/31036474/haskell-checking-if-all-list-elements-are-unique
+-- Test if it's a set (http://geekyplatypus.com/category/haskell/)
 
 --------------------------------------------------------------------------
 module Lab4 where
@@ -22,7 +26,6 @@ import SetOrd
 import Control.Monad
 
 -- From scratch
-
 genSet :: Int -> Int -> Int -> IO (Set Int)
 genSet n mini maxi
   | maxi - mini < n = error "Input out of bound"
@@ -44,22 +47,21 @@ genQuickCheckSet 0 = return emptySet
 genQuickCheckSet n = do sampleSet <- arbitrary
                         return (list2set sampleSet)
 
--- Test if it's a set (http://geekyplatypus.com/category/haskell/)
+-- Testable properties
 prop_isSet :: Set Int -> Bool
 prop_isSet (Set x) = isSet x
 -- > quickCheck (forAll (sized genQuickCheckSet) prop_isSet)
 
--- https://stackoverflow.com/questions/31036474/haskell-checking-if-all-list-elements-are-unique
 isSet :: [Int] -> Bool
 isSet []     = True
 isSet (x:xs) = x `notElem` xs && isSet xs
 
+-- Helper functions
 convertIO :: IO (Set Int) -> IO Bool
 convertIO x = do
   ioSet <- x
   return (prop_isSet ioSet)
 
--- if/then/else for IO Bool https://mail.haskell.org/pipermail/haskell-cafe/2006-November/019752.html
 if' b t e = if b then t else e
 ifM = liftM3 if'
 
