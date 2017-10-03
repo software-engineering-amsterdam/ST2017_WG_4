ccbd58584c7c03cab0784042580d4abe5ec9ddfa
Quinten Heijn <samuel.heijn@gmail.com>
2017-10-03 14:10:41 +0200

    exercise 3

---
+++ lab_5/individual/quinten_heijn/Lab5_4.hs
@@ -0,0 +1,11 @@
+-- Assignment: Lab5
+-- Exercise: 4
+-- Student: Quinten Heijn
+-- Time needed: ??
+--------------------------------------------------------------------------------
+
+module Lab5_4 where
+import Data.List
+import System.Random
+import Control.Monad
+import Lecture5
