d5bf76385b87f320629bed6f9818bcc94de74509
Quinten Heijn <samuel.heijn@gmail.com>
2017-10-10 10:46:28 +0200

    ass 1

---
+++ lab_6/individual/quinten_heijn/assignment_3.hs
@@ -0,0 +1,9 @@
+{--
+Time spent:
+Quinten:      h
+--}
+module Assignment_3 where
+
+import Data.List
+import System.Random
+import Lecture6
---
+++ lab_6/individual/quinten_heijn/assignment_7.hs
@@ -0,0 +1,9 @@
+{--
+Time spent:
+Quinten:      h
+--}
+module Assignment_7 where
+
+import Data.List
+import System.Random
+import Lecture6
