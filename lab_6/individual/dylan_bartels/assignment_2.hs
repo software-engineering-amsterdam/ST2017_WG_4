9355c6909b210235aec9cb5b7fae2cc7fe94ff55
Dylan Bartels <d_bartels@live.nl>
2017-10-10 21:00:30 +0200

    Finished ass 1

--- lab_6/individual/dylan_bartels/assignment_2.hs
+++ lab_6/individual/dylan_bartels/assignment_2.hs
@@ -6,4 +6,10 @@ module Assignment_2 where
 
 import Data.List
 import System.Random
-import Lecture6
+import Lecture6 hiding (exM)
+import Assignment_1 hiding (main)
+
+main :: IO ()
+main = do
+  print ("Some tests")
+  return ()
