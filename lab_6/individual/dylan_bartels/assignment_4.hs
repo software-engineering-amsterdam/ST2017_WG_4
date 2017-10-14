9caa980f64a1a23f927ec78a1b4cfaad6f086861
Dylan Bartels <d_bartels@live.nl>
2017-10-14 21:12:00 +0200

    Finished ass 4

--- lab_6/individual/dylan_bartels/assignment_4.hs
+++ lab_6/individual/dylan_bartels/assignment_4.hs
@@ -1,8 +1,18 @@
 {--
 Time spent:
-Dylan:      2h
+Dylan:      2.5h
 
+Info used:
 https://stackoverflow.com/questions/13337937/couldnt-match-expected-type-bool-with-actual-type-io-bool
+
+The following values were found as least composite number (~20 runs):
+k=1 - 9
+k=2 - 15
+k=3 - 33
+
+The value for k has a direct influence of the size of different values being
+used for the base of the Fermat's little theorem. The base can suggest that a
+number is a prime while it is a psuedo prime.
 --}
 module Assignment_4 where
 
@@ -12,31 +22,17 @@ import Lecture6 hiding (main, composites, exM)
 import Assignment_1 hiding (main)
 import Assignment_3
 
+main :: IO ()
+main = do
+  oneK <- leastComposite 1 composites
+  print oneK
+  twoK <- leastComposite 2 composites
+  print twoK
+  threeK <- leastComposite 3 composites
+  print threeK
+  return ()
 
-
-leastComposite :: Integer -> [Integer] -> IO Integer
+leastComposite :: Int -> [Integer] -> IO Integer
 leastComposite n (x:xs) = do
    y <- primeTestsF n x
    if y then return x else leastComposite n xs
-
-main :: IO ()
-main = do
-  let y = (map primeTestsF (fromInteger composites))
-  -- id y
-  -- x <- filter (== False) y
-  -- let x = filter (==False) y
-  return (y)
-
-leastComposite :: Integer -> Integer
-leastComposite x = if returnTrue (primeTestF
-                      (fromInteger (composites !! (fromInteger x))))
-                   then leastComposite (x+1)
-                   else composites !! (fromInteger x)
-
-leastComposite' :: Int -> IO Integer
-leastComposite' start =
-    fix $ \again -> do
-        composite <- composites
-        x <- (composite !! start)
-        success <- primeTestF x
-        if success then return x else again
